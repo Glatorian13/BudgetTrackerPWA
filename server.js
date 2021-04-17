29ae9fe3177911f02e3fed9c9e4b01029bff372f
Glatorian13 <glatorian13@gmail.com>
2021-04-17 09:59:35 -0400

    bugfix

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 // routes
